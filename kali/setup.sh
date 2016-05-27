61b96dd4908d5528fd0804df1ff50446caba1e82
a4m <a2sa2s@126.com>
2016-05-27 12:12:00 +0800

    Update setup.sh

--- kali/setup.sh
+++ kali/setup.sh
@@ -8,7 +8,7 @@ cd /opt
 git clone https://github.com/lijiejie/BBScan.git
 cd BBScan && pip install -r requirements.txt && cd /opt
 
-git clone https://github.com/lijiejie/subDoaminsBrute.git
+git clone https://github.com/lijiejie/subDomainsBrute.git
 pip install dnspython
 
 git clone https://github.com/lijiejie/GitHack.git
