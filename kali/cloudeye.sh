ed6ab846c580d7f83fd4c3f9b02a98d924aaeb90
a4m <a2sa2s@126.com>
2016-05-27 15:59:38 +0800

    Create cloudeye.sh

---
+++ kali/cloudeye.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# cloudeye is a dns/apache log recorder
+# you can buy a code at http://www.wooyun.org/market/260
+# you need edit it before you use this .sh
+
+c_eye='http://xxxxx.xxxxx.dnslog.info'
